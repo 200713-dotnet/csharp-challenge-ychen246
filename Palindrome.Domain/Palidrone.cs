0589402fe9ae8486959f2ceed3af06d2310909b0
yujun chen <ychen246@hawk.iit.edu>
2020-07-20 15:59:54 -0500

    Added Logic for character comparison

---
+++ Palindrome.Domain/Palidrone.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Palindrome.Domain
+{
+    public class Palindrome
+    {
+        public void isPalindrome(string s)
+        {
+            char charArray = s.ToCharArray();  //Discovered method online.
+            if ((charArray.Length % 2) == 0) //If Even number of character
+            {
+                char first, last;
+                Boolean match = true;
+                int counter = 1;
+                int midpoint = charArray.Length / 2;
+                
+                do{
+                    char compare = charArray[charArray[charArray.Length-counter]];
+                    if (char.ToLower(c) == char.ToLower(compare))
+                    {
+                        
+                    }
+                    else
+                    {
+                        match = false;
+                    }
+                } while(match);
+            }
+
+        }
+    }
+}
