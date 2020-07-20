dfffc8ae3c4847bfc5ddc2a75a288bb236390f99
yujun chen <ychen246@hawk.iit.edu>
2020-07-20 16:10:47 -0500

    Added Logic for odd number of character testing

--- Palindrome.Domain/Palidrone.cs
+++ Palindrome.Domain/Palidrone.cs
@@ -7,18 +7,43 @@ namespace Palindrome.Domain
         public void isPalindrome(string s)
         {
             char charArray = s.ToCharArray();  //Discovered method online.
+            //Needs to remove spaces for phrases.
             if ((charArray.Length % 2) == 0) //If Even number of character
             {
                 char first, last;
-                Boolean match = true;
-                int counter = 1;
+                Boolean end = false;
+                int counter = 0;
                 int midpoint = charArray.Length / 2;
                 
                 do{
-                    char compare = charArray[charArray[charArray.Length-counter]];
-                    if (char.ToLower(c) == char.ToLower(compare))
+                    first = charArray[counter];
+                    last = charArray[charArray.Length - 1 - counter];
+                    if (first == last)
                     {
-                        
+                        counter += 1;
+                        if(counter = midpoint-1) 
+                        {
+                            end = true; //Only need to compare half the array.
+                        }
+                    }
+                    else
+                    {
+                        end = true;
+                    }
+                } while(!end);
+            }
+            else //Odd number of character
+            {
+                 do{
+                    first = charArray[counter];
+                    last = charArray[charArray.Length - 1 - counter];
+                    if (first == last)
+                    {
+                        counter += 1;
+                        if(counter = midpoint)
+                        {
+                            match = false; //Only need to compare half the array.
+                        }
                     }
                     else
                     {
